2e073432352d9dde51a8c5aaf7fc7a3891ca3c95
hu <18820909494@163.com>
2019-03-29 21:29:14 +0800

    3.29

---
+++ at-services/src/main/java/com/at/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.at.impl;
+
+import com.at.UserService;
+import com.at.respository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+   }
